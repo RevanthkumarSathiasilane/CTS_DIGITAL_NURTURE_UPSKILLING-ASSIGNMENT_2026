55cdec83018c47f78ae2f011be15e484f9b1346f
Revanthkumar <rkrevanth2908@gmail.com>
2026-05-27 12:25:36 +0530

    Java Assignment 15-String reversal(using StringBuilder) finished

---
+++ JAVA FSE/UPSKILLING/Module 3 - Core JAVA/src/com/cts/StringReversal.java
@@ -0,0 +1,19 @@
+package src.com.cts;
+import java.util.*;
+/* 15. String Reversal
+• Objective: Manipulate strings.
+• Task: Reverse a string entered by the user.
+• Instructions:
+o Prompt the user for a string.
+o Use a loop or StringBuilder to reverse the string.
+o Display the reversed string.
+*/
+public class StringReversal {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a string: ");
+            String text = sc.nextLine();
+            String reversed = new StringBuilder(text).reverse().toString();
+            System.out.println("Reversed string: " + reversed);
+        }
+}
